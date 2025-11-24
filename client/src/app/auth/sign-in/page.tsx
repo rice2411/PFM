b731c6f736955b377487c6a471f18e58cd574c2e
PhucLH <phuclh@rikkeisoft.com>
2025-11-24 16:14:01 +0700

    [client] modify sign in page

--- client/src/app/auth/sign-in/page.tsx
+++ client/src/app/auth/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import Signin from "@/components/Auth/Signin";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,10 +10,8 @@ export const metadata: Metadata = {
 export default function SignIn() {
   return (
     <>
-      <Breadcrumb pageName="Sign In" />
-
-      <div className="rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
-        <div className="flex flex-wrap items-center">
+      <div className="rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card h-screen">
+        <div className="flex flex-wrap items-center pt-10">
           <div className="w-full xl:w-1/2">
             <div className="w-full p-4 sm:p-12.5 xl:p-15">
               <Signin />
